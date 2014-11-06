726c0ae450d2b7b6f925fdb3c8d162732bd7556b
xiaorui <xiaorui@devops.local>
2014-11-06 23:48:01 +0800

    add work

---
+++ README.md
@@ -0,0 +1,30 @@
+##非堵塞日志统计,效果很牛逼
+
+```
+ruifengyun@bj-log:/data/scribe/spider$ tailf spider-2014-11-06_00001|python ~/counter.py
+Use Ctrl + C to terminate the program
+0/0 (rps/min avg)
+200/100 (rps/min avg)
+52/84 (rps/min avg)
+48/75 (rps/min avg)
+126/85 (rps/min avg)
+84/85 (rps/min avg)
+69/83 (rps/min avg)
+83/83 (rps/min avg)
+42/78 (rps/min avg)
+128/83 (rps/min avg)
+128/87 (rps/min avg)
+70/86 (rps/min avg)
+91/86 (rps/min avg)
+40/83 (rps/min avg)
+150/87 (rps/min avg)
+81/87 (rps/min avg)
+103/88 (rps/min avg)
+56/86 (rps/min avg)
+65/85 (rps/min avg)
+125/87 (rps/min avg)
+85/87 (rps/min avg)
+104/88 (rps/min avg)
+55/86 (rps/min avg)
+36/84 (rps/min avg)
+```
---
+++ logcounter.py
@@ -0,0 +1,93 @@
+#coding:utf-8
+#!/usr/bin/env python
+
+"""非堵塞的日志统计"""
+import datetime
+import array
+import getopt
+import os
+import sys
+import time
+import fcntl
+import select
+
+
+SEP = "\n"
+SECS_1M = 60
+
+
+def usage():
+    """Usage help"""
+    print "USAGE: tail -f access_log1 access_log2 access_logN | rps"
+
+
+def parse_args():
+    """Args parser"""
+    try:
+        opts = getopt.getopt(sys.argv[1:], "h", ["help"])[0]
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+        sys.exit(-1)
+    for opt in opts:
+        if opt[0] in ("-h", "--help"):
+            usage()
+            sys.exit()
+
+
+def prepare_stdin():
+    """Makes stdin non-blocking"""
+    try:
+        fcntl.fcntl(sys.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
+    except IOError:
+        print "SORRY, could not make stdin non-blocking"
+        sys.exit(-2)
+
+
+def main():
+    """Parsing args & processing stdin"""
+    parse_args()
+    prepare_stdin()
+    buf = ""
+    total_reqs = 0
+    stat_1m = array.array('I')
+
+    print "Use Ctrl + C to terminate the program"
+
+    while True:
+        time_left, cnt = 1.0, 0
+        while time_left >= 0:
+            time_start = time.time()
+            try:
+                rlist = select.select([sys.stdin], [], [], time_left)[0]
+            except KeyboardInterrupt:
+                print "BYE, %d total reqs" % total_reqs
+                sys.exit(0)
+            if not rlist:
+                break
+            time_left -= (time.time() - time_start)
+            buf += sys.stdin.read()
+            try:
+                ridx = buf.rindex(SEP)
+            except ValueError:
+                continue
+            idx = 0
+            while True:
+                idx = buf.find(SEP, idx, ridx)
+                if idx == -1:
+                    break
+                cnt += 1
+                idx += 1
+            cnt += 1
+            buf = buf[ridx + 1:]
+
+        total_reqs += cnt
+        if len(stat_1m) == SECS_1M:
+            stat_1m.pop(0)
+        stat_1m.append(cnt)
+        print("%d/%d (rps/min avg)" % (cnt, round(float(sum(stat_1m)) /
+                                            len(stat_1m))))
+
+
+if __name__ == "__main__":
+    main()
